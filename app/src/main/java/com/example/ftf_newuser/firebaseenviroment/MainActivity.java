eb243865471bd1d0b089c40878508644b9b565b8
Ricardo Vasquez <ricardo.vasquez@poslabs.com>
2018-08-08 15:34:11 -0500

    prod

--- app/src/main/java/com/example/ftf_newuser/firebaseenviroment/MainActivity.java
+++ app/src/main/java/com/example/ftf_newuser/firebaseenviroment/MainActivity.java
@@ -3,11 +3,20 @@ package com.example.ftf_newuser.firebaseenviroment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.google.firebase.crash.FirebaseCrash;
+
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
+
+    ArrayList<String> array;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //FirebaseCrash.log("Activity created");
+        array.size();
+
     }
 }
